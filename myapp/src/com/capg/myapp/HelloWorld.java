7eeeb5e404fdf909746c30f83324fbb78a46f1b3
Lenovo <Lenovo@DESKTOP-VK3DL37>
2021-06-01 15:58:14 +0530

    demo project added

---
+++ myapp/src/com/capg/myapp/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.capg.myapp;
+
+public class HelloWorld {
+	
+	public static void main(String args[]) {
+	
+	System.out.println("Welcome To Capgemini");
+	System.out.println("Hello world");
+	
+	}
+
+}
